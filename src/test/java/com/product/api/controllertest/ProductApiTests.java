8855a0154364ebfed3cb7d70dbd350f79eb8e5b3
ugur ciftci <ugurprog@gmail.com>
2020-08-22 01:20:57 +0300

    Create ProductApiTests.java

---
+++ src/test/java/com/product/api/controllertest/ProductApiTests.java
@@ -0,0 +1,8 @@
+package com.product.api.controllertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+public class ProductApiTests {
+}
