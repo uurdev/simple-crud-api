c8f652db27e4b1b03dfc9d924750fc06ad2aadfd
ugur ciftci <ugurprog@gmail.com>
2020-08-23 23:13:12 +0300

    Update ProductRestController.java

--- src/main/java/com/product/api/controller/ProductRestController.java
+++ src/main/java/com/product/api/controller/ProductRestController.java
@@ -4,9 +4,9 @@ import com.product.api.model.Product;
 import com.product.api.service.PhotoService;
 import com.product.api.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +19,20 @@ public class ProductRestController {
     @GetMapping(value = "/products")
     public List<Product> getAll() {
         return productService.getAll();
+    }
+
+    @PostMapping(value = "/save/product")
+    public HttpStatus saveProduct(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping(value = "/delete/{id}")
+    public HttpStatus deleteProduct(@PathVariable Long id) {
+        return productService.delete(id);
+    }
+
+    @PutMapping(value = "/update/{id}")
+    public HttpStatus updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return productService.update(id, product);
     }
 }
